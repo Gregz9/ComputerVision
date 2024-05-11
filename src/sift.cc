3fd5fef455c85a2e1b907a078489520ec672529f
Gregz9 <gregorkajda@gmail.com>
2024-05-11 11:31:23 +0200

    Guassian pyramid completed, next step is the creation of the DoG pyramid

--- src/sift.cc
+++ src/sift.cc
@@ -1,39 +1,43 @@
 #include "sift.h"
 #include "filters.h"
 
-void computeGaussianPyramid(cv::Mat *img){
-
-  img = cv::resize(*img.rows*2, *img.cols*2, cv::INTER_LINEAR);
+GaussianPyramid computeGaussianPyramid(cv::Mat img){
+  cv::resize(img, img, cv::Size(img.rows*2, img.cols*2), cv::INTER_LINEAR);
   // Computing the standard deviation of the gaussian kernel
   // For the very first image, init_sigma is equivalent to: 
   float std_dev = std::sqrt((MIN_SIGMA*MIN_SIGMA) - (INP_SIGMA*INP_SIGMA)); 
   std_dev /= PX_DST_MIN;
 
   cv::Mat kernel = create1DGaussKernel(std_dev);
-  cv::sepFilter2D(*img, *img, -1, kernel, kernel);
+  cv::sepFilter2D(img, img, -1, kernel, kernel);
   
   int num_scales = NUM_SCL_PER_OCT + 3; 
-  float min_coeff = MIN_SIGMA / PX_DST_MIN;
-  int k = std::pow(k, 1/num_scales);
+  float min_coef = MIN_SIGMA / PX_DST_MIN;
+  auto k = static_cast<float>(std::pow(2, 1.0/num_scales));
 
   std::vector<float> std_devs {std_dev};
-  GaussianPyramid pyramid; 
+  GaussianPyramid pyramid;
   pyramid.num_scales_per_oct = num_scales;
 
   // Computing standard deviations
   for (int i=1; i<num_scales; ++i){
-    prev_sigma = std::pow(k, i-1) * min_coeff;
-    sigma = k * prev_sigma; 
+    auto prev_sigma = static_cast<float>(std::pow(k, i-1) * min_coef);
+    float sigma = k * prev_sigma;
     std_devs.push_back(std::sqrt((sigma*sigma) - (prev_sigma*prev_sigma)));
   }
-  
-  pyramid.imgs.reserve(num_scales*NUM_OCT);
-  for (int i=0; i<NUM_OCT; ++i){
-    pyramid.imgs.push_back(*img);
-    for(int j=0; j<num_scales; ++j){
-      cv::Mat scale_img = pyramid.imgs.at((i*num_scales)-1);
-      
-    }
-  } 
 
+  pyramid.imgs.reserve(num_scales*NUM_OCT);
+  for (int i=0; i<NUM_OCT; i++){
+    pyramid.imgs.push_back(img.clone());
+
+    for(int j=1; j < (int)std_devs.size();j++){
+      const cv::Mat& scale_img = pyramid.imgs.at(i*num_scales);
+      cv::sepFilter2D(scale_img, scale_img, -1, std_devs[j], std_devs[j]);
+      pyramid.imgs.push_back(scale_img.clone());
+    }
+    const cv::Mat& next_img = pyramid.imgs.at((i*num_scales)+NUM_SCL_PER_OCT);
+    cv::resize(next_img, img, cv::Size(img.rows/2, img.cols/2), cv::INTER_LINEAR);
+
+  }
+return pyramid;
 }
--- src/sift.h
+++ src/sift.h
@@ -2,14 +2,15 @@
 
 // #include "opencv2/core.hpp"
 #include <vector>
-#include <opencv4/opencv2/core.hpp>
-#include <opencv4/opencv2/core/mat.hpp>
-#include <opencv4/opencv2/core/types.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/core/mat.hpp>
+#include <opencv2/core/types.hpp>
+#include <opencv2/opencv.hpp>
 #include <stdlib.h>
 
 constexpr int NUM_OCT = 5; 
 constexpr int NUM_SCL_PER_OCT = 3; 
-constexpr float MIN_SIGMA = 0.8; // Minimal blur level 
+constexpr float MIN_SIGMA = 0.8; // Minimal blur level
 constexpr float INP_SIGMA = 0.5; // Assumed blur level of input image
 constexpr float IN_PX_DST = 1.0; 
 constexpr float PX_DST_MIN = 0.5;
@@ -19,19 +20,15 @@ constexpr float MAX_REF_THR = 0.5;
 constexpr float DOG_THR = 0.015;
 constexpr float EDGE_THR = 0.f;
 
-typedef std::vector<Mat_<float>> imageVector; 
-
-std::vector<cv::Point> point; 
-
-struct {
+struct GaussianPyramid{
   int num_oct = NUM_OCT;
   int num_scales_per_oct;
   std::vector<cv::Mat> imgs{}; 
-} GaussianPyramid; 
+};
 
-void computeDogThr(); 
+void computeDogThr();
 
-void computeGaussianPyramid(cv::Mat *img); 
+GaussianPyramid computeGaussianPyramid(cv::Mat img);
 
 void bilinearInterpolation(); 
 
